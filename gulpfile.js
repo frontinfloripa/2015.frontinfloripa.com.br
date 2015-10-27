dd248b2b3e8817df4ae15db49bf20ac30d494162
Diogo Moretti <diogo.pearljam@gmail.com>
2015-10-27 00:19:57 -0200

    Add website of 2015

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,71 @@
-// Load Gulp and your plugins
-var gulp    = require('gulp'),
-    connect = require('gulp-connect'),
-    stylus  = require('gulp-stylus'),
-    plumber = require('gulp-plumber');
+// -- Load Gulp and your plugins
+var gulp       = require('gulp'),
+    connect    = require('gulp-connect'),
+    stylus     = require('gulp-stylus'),
+    handlebars = require('gulp-compile-handlebars'),
+    rename     = require('gulp-rename'),
+    ghPages    = require('gulp-gh-pages'),
+    plumber    = require('gulp-plumber'),
+    yaml       = require('js-yaml'),
+    fs         = require('fs');
 
+// -- Settings
 var paths = {
-    styles: 'src/stylus/**/*',
-    html:   '*.html'
+    dist:   './dist',
+    src:    './src'
 };
 
-// Connect task
+var watches = {
+    stylus:     paths.src + '/stylus/**/*.styl',
+    handlebars: paths.src + '/handlebars/**/*.hbs'
+};
+
+var files = {
+    stylus:     paths.src + '/stylus/*.styl',
+    handlebars: paths.src + '/handlebars/*.hbs'
+};
+
+// -- Connect task
 gulp.task('connect', connect.server({
-    root: __dirname + '/',
+    root: paths.dist,
     port: 5000,
-    livereload: true,
-    open: true
+    livereload: true
 }));
 
-// HTML task
-gulp.task('html', function () {
-    gulp.src('*.html')
+// -- Handlebars (HTML) task
+gulp.task('handlebars', function () {
+    var config = yaml.safeLoad(fs.readFileSync('./config.yml', 'utf-8'));
+    
+    gulp.src(files.handlebars)
+        .pipe(handlebars(config))
+        .pipe(rename('index.html'))
+        .pipe(gulp.dest(paths.dist))
         .pipe(connect.reload());
 });
 
-// Stylus task
+// -- Stylus (CSS) task
 gulp.task('stylus', function () {
-    gulp.src('./src/stylus/*.styl')
+    gulp.src(files.stylus)
         .pipe(plumber())
         .pipe(stylus({
-            use: ['nib'], 
-            //set: ['compress']
+            use: ['nib'],
+            set: ['compress']
         }))
-        .pipe(gulp.dest('./assets/css'))
+        .pipe(gulp.dest(paths.dist + '/assets/css'))
         .pipe(connect.reload());
 });
 
-// Watch task
-gulp.task('watch', function () {
-    gulp.watch(paths.styles, ['stylus']);
-    gulp.watch(paths.html, ['html']);
+// -- Deploy to gh-pages task
+gulp.task('deploy', function() {
+  return gulp.src(paths.dist + '/**/*')
+    .pipe(ghPages());
 });
 
-// Set 'gulp server' for development
-gulp.task('server', ['connect', 'stylus', 'watch']);
+// -- Watch task
+gulp.task('watch', function () {
+    gulp.watch(watches.stylus, ['stylus']);
+    gulp.watch(watches.handlebars, ['handlebars']);
+});
+
+// -- Set 'gulp server' for development
+gulp.task('server', ['handlebars', 'stylus', 'connect', 'watch']);
